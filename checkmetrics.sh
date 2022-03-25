7f607529e2c9b0637bd2340bee956fef3165055f
SWAPBROWSER COMPANY.COM <61902110+P7-33@users.noreply.github.com>
2022-03-25 07:49:22 -0500

    Update checkmetrics.sh

--- checkmetrics.sh
+++ checkmetrics.sh
@@ -9,7 +9,7 @@ fi
 lint=$($1 check metrics < "$2" 2>&1 | grep -v -E "^node_(entropy|memory|netstat|wifi_station)_")
 
 if [[ -n $lint ]]; then
-    echo -e "Some Prometheus metrics do not follow best practices:\n"
+    echo -e "Some stakin-eus metrics do not follow best practices:\n"
     echo "$lint"
 
     exit 1
